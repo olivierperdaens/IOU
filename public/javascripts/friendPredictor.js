7b43a4abc32c6e31c114bbdddc4c2b19a3570104
Lobster <omar.kraim@student.uclouvain.be>
2018-05-17 20:15:58 +0200

    friend predictor

---
+++ public/javascripts/friendPredictor.js
@@ -0,0 +1,16 @@
+function  probabilityFinder( friendMatrix  ){
+    var baseArray = friendMatrix[0].slice();
+    //copies the first array to to base
+
+    for (var i = 0 ; i < friendMatrix[0].length ; i++){
+        for( var j =0 ; j < friendMatrix[i].length; j++ ){
+
+
+
+        }
+
+
+
+    }
+
+}
